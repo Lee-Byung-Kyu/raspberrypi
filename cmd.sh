7317fab6df44110a1902c0eb0f3998a5ee5e2d1e
Lee-Byung-Kyu <leebang54@khu.ac.kr>
2022-11-07 21:19:16 +0900

    add : setup step

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,7 @@
 # mosquitto Broker init
+sudo mkdir /var/run/mosquitto/
+sudo chown mosquitto: /var/run/mosquitto
 sudo mosquitto -c /etc/mosquitto/mosquitto.conf
 
 # mosquitto subscriber init (for test)
 mosquitto_sub -h 192.168.0.11 -t /test
-
--- humi_temp.py
+++ humi_temp.py
@@ -14,6 +14,7 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin);
     if humidity is not None and temperature is not None:
         client.publish('/test', json.dumps({"temperature": temperature, "humidity": humidity}), 1)
+        print("well published")
     else:
         print('Failed to get reading. Try again!')
         client.disconnect()
